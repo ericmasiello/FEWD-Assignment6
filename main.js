e0551e133c96b89e5d0b9df497c5fe476c10197a
Eric Masiello <eric.j.masiello@gmail.com>
2017-04-18 20:47:08 -0400

    solution

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('form').submit(onSubmitForm);
+
+  function onSubmitForm(event) {
+    event.preventDefault();
+
+    var city = $('#city-type').val().toLowerCase();
+    var bodyClass;
+
+    if (city === 'new york' || city === 'new york city' || city === 'nyc') {
+      bodyClass = 'nyc';
+    } else if (city === 'san francisco' || city === 'sf' || city === 'bay area') {
+      bodyClass = 'sf';
+    } else if (city === 'los angeles' || city === 'la' || city === 'lax') {
+      bodyClass = 'la';
+    } else if (city === 'austin' || city === 'atx') {
+      bodyClass = 'austin';
+    } else if (city === 'sydney' || city === 'syd') {
+      bodyClass = 'sydney';
+    }
+
+    $('body').removeClass('nyc sf la atx sydney').addClass(bodyClass);
+  }
+});
