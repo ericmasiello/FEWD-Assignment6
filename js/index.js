e36f9ac9921bc84712f39bc9cbdc2cc72f44c2a0
Matthew Mueller <mmueller@RealMagnet.local>
2017-04-21 16:20:49 -0400

    add trailing space fix

--- js/index.js
+++ js/index.js
@@ -8,24 +8,28 @@ $('document').ready(function(){
 	function hitSubmit(event) {
 		event.preventDefault();
 
-		var userInput = $('#city-type').val();
-			userInput.toLowerCase();
+		var userInput = $('#city-type').val().trim().toLowerCase();
+
 
 			if (userInput ==='nyc'||userInput ==='new york city'
 				||userInput ==='new york'){
 						$('body').removeClass('sf la austin sydney').addClass('nyc');
 						$('#city-type').val('');
+
 			} else if (userInput ==='san francisco'||userInput ==='sf'
 				||userInput ==='bay area'){
 						$('body').removeClass('nyc la austin sydney').addClass('sf');
 						$('#city-type').val('');
-			} else if (userInput ==='os angeles'||userInput ==='la'
+
+			} else if (userInput ==='los angeles'||userInput  ==='la'
 				||userInput ==='lax'){
 						$('body').removeClass('nyc sf austin sydney').addClass('la');
 						$('#city-type').val('');
+
 			} else if (userInput ==='austin'||userInput ==='atx'){
 						$('body').removeClass('nyc sf la sydney').addClass('austin');
 						$('#city-type').val('');
+
 			} else if (userInput ==='sydney'||userInput ==='syd'){
 						$('body').removeClass('nyc sf la austin').addClass('sydney');
 						$('#city-type').val('');
@@ -33,3 +37,5 @@ $('document').ready(function(){
 	}
 
 }); 
+
+
