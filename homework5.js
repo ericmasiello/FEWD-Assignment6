f0013005fb3a23aef9970f8f491b687aecf2f52b
SequoiaRH <morganpowell@Morgans-MacBook-Air.local>
2017-04-24 20:18:56 -0400

    REsubmit

--- homework5.js
+++ homework5.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
 	console.log('this is running');
-});
+
 
 $('#submit-btn').click(function(event) {
 	event.preventDefault()
@@ -9,50 +9,16 @@ $('#submit-btn').click(function(event) {
 
 	if (city === 'nyc'|| city === 'new york'||city === 'ny' || city === 'new york city') {
 	$('body').removeClass('nyc sf la austin sydney').addClass('nyc');
-}
-});
-
-$('#submit-btn').click(function(event) {
-	event.preventDefault()
-	var city = $('#city-type').val();
-	city = (city.toLowerCase());
-
-	if (city === 'la'|| city === 'los angeles'|| city === 'lax') {
+} else if (city === 'la'|| city === 'los angeles'|| city === 'lax') {
 	$('body').removeClass('nyc sf la austin sydney').addClass('la');
-}
-
-});
-
-$('#submit-btn').click(function(event) {
-	event.preventDefault()
-	var city = $('#city-type').val();
-	city = (city.toLowerCase());
-	
-	if (city === 'sf' || city === 'bay area' || city === 'san francisco') {
+} else if (city === 'sf' || city === 'bay area' || city === 'san francisco') {
 		$('body').removeClass('nyc sf la austin sydney').addClass('sf');
-}
-
-});
-
-$('#submit-btn').click(function(event) {
-	event.preventDefault()
-	var city = $('#city-type').val();
-	city = (city.toLowerCase());
-
-	if (city === 'austin' || city === 'atx') {
+} else if (city === 'austin' || city === 'atx') {
 		$('body').removeClass('nyc sf la austin sydney').addClass('austin');
-}
-
-});
-
-$('#submit-btn').click(function(event) {
-	event.preventDefault()
-	var city = $('#city-type').val();
-	city = (city.toLowerCase());
-
-	if (city === 'sydney' || city === 'syd') {
+} else if (city === 'sydney' || city === 'syd') {
 		$('body').removeClass('nyc sf la austin sydney').addClass('sydney');
 }
 
+});
 
 });
