0ffcc0434e3458724a18fdcb37d53e94a97440ad
Eric Masiello <eric.j.masiello@gmail.com>
2018-05-01 18:13:51 -0400

    Fix up main.js

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-$(document).ready(function() {
-  $('form').submit(onSubmitForm);
+/* global $ */
+function onSubmitForm(event) {
+  event.preventDefault();
 
-  function onSubmitForm(event) {
-    event.preventDefault();
+  var city = $('#city-type').val();
+  city = city.toLowerCase();;
+  var bodyClass = '';
 
-    var city = $('#city-type').val().toLowerCase();
-    var bodyClass = '';
-
-    if (city === 'new york' || city === 'new york city' || city === 'nyc') {
-      bodyClass = 'nyc';
-    } else if (city === 'san francisco' || city === 'sf' || city === 'bay area') {
-      bodyClass = 'sf';
-    } else if (city === 'los angeles' || city === 'la' || city === 'lax') {
-      bodyClass = 'la';
-    } else if (city === 'austin' || city === 'atx') {
-      bodyClass = 'austin';
-    } else if (city === 'sydney' || city === 'syd') {
-      bodyClass = 'sydney';
-    }
-    
-    // Remove all the different city classes from the 'body'
-    // then just add the one that matched
-    $('body').removeClass('nyc sf la austin sydney').addClass(bodyClass);  
+  if (city === 'new york' || city === 'new york city' || city === 'nyc') {
+    bodyClass = 'nyc';
+  } else if (city === 'san francisco' || city === 'sf' || city === 'bay area') {
+    bodyClass = 'sf';
+  } else if (city === 'los angeles' || city === 'la' || city === 'lax') {
+    bodyClass = 'la';
+  } else if (city === 'austin' || city === 'atx') {
+    bodyClass = 'austin';
+  } else if (city === 'sydney' || city === 'syd') {
+    bodyClass = 'sydney';
   }
-});
+  
+  // Remove all the different city classes from the 'body'
+  // then just add the one that matched
+  // alternatively, calling removeClass() will remove call classes
+  $('body').removeClass('nyc sf la austin sydney').addClass(bodyClass);  
+}
+
+$('form').submit(onSubmitForm);
